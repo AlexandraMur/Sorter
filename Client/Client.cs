05a1b10a4a5da428c629fad381a05e7dd740c33e
Sasha <murzina93@gmail.com>
2016-03-26 06:42:31 +0600

    add Client code

--- Client/Client.cs
+++ Client/Client.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Client.sorter;
 
 namespace Client
 {
     class Client
     {
+        private const int SIZE = 15;
+
         static void Main(string[] args)
         {
+            WCFSorterClient client = new WCFSorterClient();
 
+            int[] unsorted_array = new int[SIZE];
+
+            for (int i = 0; i < SIZE; i++) 
+            { 
+                unsorted_array[i] = (new Random()).Next(100);
+                Console.Write(unsorted_array[i]);
+            }
+
+            int[] sorted_array = client.Sort(unsorted_array);
+            for (int i = 0; i < SIZE; i++)
+            {
+                Console.Write(sorted_array[i]);
+            }
         }
     }
 }
